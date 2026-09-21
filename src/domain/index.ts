b494f03b8c45d7d05f32c30cbec1638aefda5b76
setnessconsulting <119264110+setnessconsulting@users.noreply.github.com>
2026-09-21 13:24:44 -0700

    GAME-340: export deterministic weather kernel contract

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,4 +1,52 @@
 export { createSeededRng } from "./seededRng";
 export type { SeededRng } from "./seededRng";
 
-export const DOMAIN_CONTRACT_VERSION = "0.1.0-foundation";
+export { deterministicSignedNoise } from "./deterministicNoise";
+export {
+  advanceScenario,
+  getAvailableForecastWindows,
+  getScenarioOutcomeFacts,
+  initializeScenario,
+  stateAtMinute
+} from "./weatherKernel";
+export {
+  parseReplayTrace,
+  replayScenario,
+  serializeReplayTrace
+} from "./replay";
+export type {
+  AdvanceReplayAction,
+  ReplayAction,
+  ReplayCheckpoint,
+  ReplayTrace
+} from "./replay";
+export {
+  assertSaneObservation,
+  assertValidKernelScenario,
+  DomainScenarioError
+} from "./weatherValidation";
+export type {
+  AirMassDefinition,
+  AirMassState,
+  BoundaryDefinition,
+  BoundaryState,
+  ForecastWindow,
+  KernelScenarioDefinition,
+  NormalizedPoint,
+  ObservationDimension,
+  ObservationNoiseRule,
+  PrecipitationCellDefinition,
+  PrecipitationCellState,
+  ScenarioOutcomeFacts,
+  ScenarioProgress,
+  ScenarioSchemaVersion,
+  ScenarioState,
+  StationChangeFact,
+  StationDefinition,
+  StationEffectRule,
+  StationObservation,
+  StationObservationDelta,
+  TimelineDefinition
+} from "./weatherTypes";
+
+export const DOMAIN_CONTRACT_VERSION = "0.2.0-wc03";
